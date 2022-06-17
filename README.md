5e080003a4009b0f1ba50886e7f2178f801fc04b
Roman Timoshkov <timoshkow@mail.ru>
2022-06-17 11:39:41 +0300

    making edits #1

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/0uf8gh8uda9160l8?svg=true)](https://ci.appveyor.com/project/RTimoshkow/ordering-card)
--- src/test/java/ru/netology/web/WebInterfaceTest.java
+++ src/test/java/ru/netology/web/WebInterfaceTest.java
@@ -1,5 +1,6 @@
 package ru.netology.web;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -8,42 +9,61 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class WebInterfaceTest {
 
+    @BeforeEach
+    void openBrowser() {
+        open("http://localhost:9999");
+    }
+
     @Test
     void shouldValidValues() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Роман");
         $("[data-test-id=phone] input").setValue("+12345678901");
         $("[data-test-id=agreement]").click();
         $(".button").click();
-        $(".Success_successBlock__2L3Cw").shouldHave(text("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=\"order-success\"]").shouldHave(text("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
     void shouldInvalidNameValue() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Roman");
         $("[data-test-id=phone] input").setValue("+12345678901");
         $("[data-test-id=agreement]").click();
         $(".button").click();
-        $(".input_invalid").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void shouldInvalidTelephoneValue() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Роман");
         $("[data-test-id=phone] input").setValue("12345678901");
         $("[data-test-id=agreement]").click();
         $(".button").click();
-        $(".input_invalid").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     void shouldUncheckedCheckBox() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Роман");
         $("[data-test-id=phone] input").setValue("+12345678901");
         $(".button").click();
-        $(".input_invalid").shouldHave(text("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+        $("[data-test-id='agreement'].input_invalid .checkbox__text").shouldHave(text("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
+
+    @Test
+    void shouldNullValueOfTheName() {
+//        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("+12345678901");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldZeroPhoneValue() {
+        $("[data-test-id=name] input").setValue("Роман");
+//        $("[data-test-id=phone] input").setValue("");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(text("Поле обязательно для заполнения"));
     }
 }
